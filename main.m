13934a2e77b49cd8b5f7d662449a267b8bcaef08
Marco Amato <amato.marco.1993@gmail.com>
2026-09-25 11:23:34 +0200

    wip

--- main.m
+++ main.m
@@ -11,8 +11,8 @@
 
 cleanmemory();
 
-inputFileDirectory = '/home/marco/Documents/MPFEM/inputfiles';
-outputFileDirectory = '/home/marco/Documents/MPFEM/outputfiles';
+inputFileDirectory = '/home/marco/Documents/MPFEMFiles/inputfiles';
+outputFileDirectory = '/home/marco/Documents/MPFEMFiles/outputfiles';
 
 inputFileName = 'ret2d';
 outputFileName = 'ret2d';
